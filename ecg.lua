3c5695e9de104b5540ac361f0110a3d0e6ee8299
Stephen Irons <stephen.irons@clear.net.nz>
2013-02-14 09:19:02 +1300

    Put pack the output.

--- ecg.lua
+++ ecg.lua
@@ -1,11 +1,11 @@
 #! /usr/bin/lua
 
 local pcsv = require 'physionet_csv'
---local pretty = require 'pl.pretty'
+local pretty = require 'pl.pretty'
 
 waveforms1 = pcsv.read('data/sim/ecg_1')
 waveforms2 = pcsv.read('data/sim/ecg_2')
 
---pretty.dump(waveforms1)
---pretty.dump(waveforms2)
+pretty.dump(waveforms1)
+pretty.dump(waveforms2)
 
