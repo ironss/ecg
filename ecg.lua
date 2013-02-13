fbc25074fa8bbb2e65ece4a4278bffb2e540adcb
Stephen Irons <stephen.irons@clear.net.nz>
2013-02-14 10:03:54 +1300

    Added path to libraries.

--- ecg.lua
+++ ecg.lua
@@ -1,4 +1,7 @@
-#! /usr/bin/lua
+#! /usr/bin/env gsl-shell
+
+package.path = package.path .. ';/usr/share/lua/5.1/?.lua'
+package.cpath = package.cpath .. ';/usr/lib/x86_64-linux-gnu/lua/5.1/?.so'
 
 local physionet = require 'physionet'
 local pretty = require 'pl.pretty'
@@ -6,6 +9,10 @@ local pretty = require 'pl.pretty'
 waveforms1 = physionet.read_csv('data/sim/ecg_1')
 waveforms2 = physionet.read_csv('data/sim/ecg_2')
 
-pretty.dump(waveforms1)
-pretty.dump(waveforms2)
+if pretty ~= nil then
+   pretty.dump(waveforms1)
+   pretty.dump(waveforms2)
+end
+
+
 
