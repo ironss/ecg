17f34f98220dccff420c28a78026c7ae8482e28a
Stephen Irons <stephen.irons@clear.net.nz>
2013-02-14 11:47:12 +1300

    Draw multiple traces, nicely spaced out.

--- ecg.lua
+++ ecg.lua
@@ -6,8 +6,9 @@ package.cpath = package.cpath .. ';/usr/lib/x86_64-linux-gnu/lua/5.1/?.so'
 local physionet = require 'physionet'
 local pretty = require 'pl.pretty'
 
-waveforms1 = physionet.read_csv('data/sim/ecg_1')
-waveforms2 = physionet.read_csv('data/sim/ecg_2')
+waveforms1 = physionet.read_csv(arg[1])
+--waveforms1 = physionet.read_csv('data/sim/ecg_10')
+--waveforms2 = physionet.read_csv('data/sim/ecg_2')
 
 if pretty ~= nil then
 --   pretty.dump(waveforms1)
@@ -15,13 +16,23 @@ if pretty ~= nil then
 end
 
 
-function graph.tiplot(table, a, b, color)
-   return graph.fiplot(function(i) return table[i] end, a, b, color)
+function graph.tiline(table, a, b)
+   return graph.filine(function(i) return table[i] end, a, b)
 end
 
 
-p = graph.tiplot(waveforms1[1].samples, 1, waveforms1[1].nsamples, red)
-p:show()
+local waveform = waveforms1
+
+local plot = graph.canvas('ECG: ' .. waveform.name)
+plot:limits(0, -500, waveform[1].nsamples, 750*12)
+
+for i = 1, waveform.ntraces do
+   local line = graph.tiline(waveform[i].samples, 1, waveform[i].nsamples)
+   plot:add(line, 'blue', {{'stroke', width=1}, {'translate', x=0, y=0  }, }, {{'translate', x=0, y=(i-1)*750 }, })
+end
+
+plot:flush()
+plot:show()
 
 
 while true do
