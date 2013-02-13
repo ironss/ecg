e9ccb7bd7011f8a5ce583d3f53d3e4f6c67b0def
Stephen Irons <stephen.irons@clear.net.nz>
2013-02-14 10:16:58 +1300

    Display the whole trace.

--- ecg.lua
+++ ecg.lua
@@ -10,9 +10,19 @@ waveforms1 = physionet.read_csv('data/sim/ecg_1')
 waveforms2 = physionet.read_csv('data/sim/ecg_2')
 
 if pretty ~= nil then
-   pretty.dump(waveforms1)
-   pretty.dump(waveforms2)
+--   pretty.dump(waveforms1)
+--   pretty.dump(waveforms2)
 end
 
 
+function graph.tiplot(table, a, b, color)
+   return graph.fiplot(function(i) return table[i] end, a, b, color)
+end
 
+
+p = graph.tiplot(waveforms1[1].samples, 1, waveforms1[1].nsamples, red)
+p:show()
+
+
+while true do
+end
