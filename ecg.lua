cf4cc045dd49e748866be677885d78d526b93834
Stephen Irons <stephen.irons@clear.net.nz>
2013-02-14 14:11:38 +1300

    Pass filename on command line.

--- ecg.lua
+++ ecg.lua
@@ -4,17 +4,12 @@ package.path = package.path .. ';/usr/share/lua/5.1/?.lua'
 package.cpath = package.cpath .. ';/usr/lib/x86_64-linux-gnu/lua/5.1/?.so'
 
 local physionet = require 'physionet'
-local pretty = require 'pl.pretty'
 
-waveforms1 = physionet.read_csv(arg[1])
---waveforms1 = physionet.read_csv('data/sim/ecg_10')
---waveforms2 = physionet.read_csv('data/sim/ecg_2')
+local filename = arg[1] or 'data/sim/ecg_1'
+waveforms1 = physionet.read_csv(filename)
 
-if pretty ~= nil then
---   pretty.dump(waveforms1)
---   pretty.dump(waveforms2)
-end
-
+--local pretty = require 'pl.pretty'
+--pretty.dump(waveforms1)
 
 function graph.tiline(table, a, b)
    return graph.filine(function(i) return table[i] end, a, b)
@@ -33,7 +28,6 @@ end
 
 plot:flush()
 plot:show()
-
 
 while true do
 end
