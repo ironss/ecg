816a97de386fc4e85db86bc2c683bc1df8edec0c
Stephen Irons <stephen.irons@clear.net.nz>
2013-02-14 15:57:46 +1300

    Display a part of the screen.

--- ecg.lua
+++ ecg.lua
@@ -18,11 +18,30 @@ end
 
 local waveform = waveforms1
 
+local start = 3000 -- 0
+local width = 4000 -- waveform[1].nsamples
+
+
+if width > waveform[1].nsamples then
+   width = waveform[1].nsamples
+end
+
+if start < 0 then 
+   start = 0 
+end
+
+if start + width > waveform[1].nsamples then
+   start = waveform[1].nsamples - width
+end
+
+
+local view = { start, -500, start+width, 750*12 }
+
 local plot = graph.canvas('ECG: ' .. waveform.name)
-plot:limits(0, -500, waveform[1].nsamples, 750*12)
+plot:limits(view[1], view[2], view[3], view[4])
 
 for i = 1, waveform.ntraces do
-   local line = graph.tiline(waveform[i].samples, 1, waveform[i].nsamples)
+   local line = graph.tiline(waveform[i].samples, start, start+width)
    plot:add(line, 'blue', {{'stroke', width=1}, {'translate', x=0, y=0  }, }, {{'translate', x=0, y=(i-1)*750 }, })
 end
 
@@ -31,3 +50,4 @@ plot:show()
 
 while true do
 end
+
